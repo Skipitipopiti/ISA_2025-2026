f41f2f35839eed4c1dd8368a8cce05234746b6ab
Nicolò Benegiamo <nico@MacBook-Air-di-Nicolo.local>
2025-11-23 19:05:58 +0100

    migliorata forma booth_encoder

--- LAB2/R4-MBE/vhdl/booth_encoder.vhd
+++ LAB2/R4-MBE/vhdl/booth_encoder.vhd
@@ -15,26 +15,21 @@ architecture behavioral of booth_encoder is
     signal a_n : signed(7 downto 0);
 begin
     a_n <= not(a);
+    s <= be_in(2); -- sign bit output
     process(a, be_in)
     begin
         case be_in is
             when "000" | "111" =>
-                s <= '0';
                 p <= (others => '0');
             when "001" | "010" =>
-                s <= '0';
                 p <= '0' & a;
             when "011" =>
-                s <= '0';
                 p <= '0' & shift_left(a, 1) ;
             when "100" =>
-                s <= '1';
                 p <= '1' & shift_left(a_n, 1);
             when "101" | "110" =>
-                s <= '1';
                 p <= '1' & a_n;
             when others =>
-                s <= '1';
                 p <= (others => '0');
         end case;
     end process;
